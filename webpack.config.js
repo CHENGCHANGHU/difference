1538a3883070f8fae0f26889c0150144323a5b63
Changhu Cheng <changhu.cheng@shopee.com>
2022-09-01 23:49:12 +0800

    feat: version 0.0.3

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require('path');
+const package = require('./package.json');
+const externalDependencies = [
+  ...Object.keys(package.dependencies || {}),
+  ...Object.keys(package.peerDependencies || {}),
+];
 
 // console.log(process.argv);
 console.log('mode:', process.env.mode);
@@ -22,6 +27,27 @@ function getWebpackConfig () {
         type: 'umd',
       },
     },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: path.join(__dirname, '..', 'node_modules')
+        },
+      ],
+    },
+    externals: [
+      ({ request }, callback) => {
+        if (externalDependencies.some(dep => dep === request || request.startsWith(`${dep}/`))) {
+          return callback(null, {
+            commonjs2: request,
+            commonjs: request,
+            module: request,
+          });
+        }
+        callback();
+      },
+    ],
   };
 }
 
